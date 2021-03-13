e525754770ade60040150f526e574611207339c0
Richard Belleville <j.belleville.richard@gmail.com>
2021-03-12 16:25:09 -0800

    Initial commit

---
+++ pagecacher.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+    "crypto/sha256"
+    "encoding/base32"
+    "net/http"
+    "net/url"
+    "golang.org/x/net/html"
+    "os"
+    "strings"
+    "fmt"
+)
+
+const mainUrl = "https://docs.openshift.com/container-platform/3.4/install_config/upgrading/manual_upgrades.html"
+
+// TODO: Programmatically generate.
+const outFileName = "cached.html"
+
+// TODO: Get this from config somehow.
+const baseName = "http://cache/c/"
+
+var linkAttrs = map[string][]string{
+    "a": []string{"href"},
+    "link": []string{"href"},
+    "meta": []string{"content"},
+    "script": []string{"src"},
+}
+
+// URL is assumed to be a normalized absolute URL.
+func hashUrl(url string) string {
+    fmt.Println("Hashing URL ", url)
+    bytes := sha256.Sum256([]byte(url))
+    rawEncoding :=  base32.StdEncoding.EncodeToString(bytes[:])
+    return strings.ToLower(rawEncoding[:len(rawEncoding)-4])
+}
+
+// Should we do a sha256 hash or what?
+func translateCachedUrl(toTranslate string, baseUrl *url.URL ) (string, error) {
+    parsedUrl, err := url.Parse(toTranslate)
+    if err != nil {
+        return "", err
+    }
+    var absoluteUrl *url.URL
+    if !parsedUrl.IsAbs() {
+        absoluteUrl = baseUrl.ResolveReference(parsedUrl)
+    } else {
+        absoluteUrl = parsedUrl
+    }
+    return baseName + hashUrl(absoluteUrl.String()), nil
+}
+
+func modifyLink(tag string, node *html.Node, baseUrl *url.URL) {
+    for i, attr := range node.Attr {
+        for _, linkAttr := range linkAttrs[tag] {
+            if attr.Key == linkAttr {
+                // TODO: Modify the link properly.
+                // TODO: Add to queue.
+                translated, err := translateCachedUrl(node.Attr[i].Val, baseUrl)
+                if err != nil {
+                    fmt.Println("Failed to parse as URL.")
+                    continue
+                }
+                node.Attr[i].Val = translated
+            }
+        }
+    }
+}
+
+// TODO: Return err.
+func cachePage(urlToCache string) {
+    resp, err := http.Get(urlToCache)
+    if err != nil {
+        println("Failed to get url %s: %v", urlToCache, err)
+        os.Exit(1)
+    }
+    // TODO: Inspect the MIME type and respond appropriately.
+    // TODO: Add a database backend.
+    outfile, err := os.Create(outFileName)
+    if err != nil {
+        println("Failed to open file %s: %v", outFileName, err)
+        os.Exit(1)
+    }
+    defer outfile.Close()
+
+    parsedUrl, parseErr := url.Parse(urlToCache)
+    if parseErr != nil {
+        fmt.Println("Failed to parse URL %s", parsedUrl)
+        os.Exit(1)
+    }
+
+    // Gathers other links that need to be fetched and modifies their hyperlinks.
+    var visitNode func(node *html.Node)
+    visitNode = func(node *html.Node) {
+        if node.Type == html.ElementNode {
+            if _, ok := linkAttrs[node.Data]; ok {
+                modifyLink(node.Data, node, parsedUrl)
+            }
+        }
+        for c := node.FirstChild; c != nil; c = c.NextSibling {
+            visitNode(c)
+        }
+    }
+    doc, err := html.Parse(resp.Body)
+    if err != nil {
+        println("Failed to parse HTML: %v", err)
+        os.Exit(1)
+    }
+    visitNode(doc)
+    html.Render(outfile, doc)
+}
+
+func main() {
+    cachePage(mainUrl)
+}
